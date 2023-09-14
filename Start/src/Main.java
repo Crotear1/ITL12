d78d47cde0543f5b7aeb7509fcd47e4951a322b7
Crotear1 <138115674+Crotear1@users.noreply.github.com>
2023-09-14 15:24:05 +0200

    wip

--- Start/src/Main.java
+++ Start/src/Main.java
@@ -1,3 +1,4 @@
+import konto.Girokonto;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -54,113 +55,113 @@ public class Main {
                     break;
                 case 2:
                     /**
-                    System.out.println("Kontonummer: ");
-                    String kontonummer2 = scanner.next();
-                    boolean kontoGefunden = false;
-                    for (Konto konto : konten) {
-                        if (konto.getKontonummer().equals(kontonummer2)) {
-                            konten.remove(konto);
-                            kontoGefunden = true;
-                            System.out.println("Konto erfolgreich aufgelöst.");
-                            break;
-                        }
-                    }
-                    if (!kontoGefunden) {
-                        System.out.println("Fehler: Konto nicht gefunden.");
-                    }
-                    break;
-                        **/
+                     System.out.println("Kontonummer: ");
+                     String kontonummer2 = scanner.next();
+                     boolean kontoGefunden = false;
+                     for (Konto konto : konten) {
+                     if (konto.getKontonummer().equals(kontonummer2)) {
+                     konten.remove(konto);
+                     kontoGefunden = true;
+                     System.out.println("Konto erfolgreich aufgelöst.");
+                     break;
+                     }
+                     }
+                     if (!kontoGefunden) {
+                     System.out.println("Fehler: Konto nicht gefunden.");
+                     }
+                     break;
+                     **/
                 case 3:
                     /**
-                    System.out.println("Kontonummer: ");
-                    String kontonummer3 = scanner.next();
-                    System.out.println("Betrag: ");
-                    double betrag3 = scanner.nextDouble();
-                    kontoGefunden = false;
-                    for (Konto konto : konten) {
-                        if (konto.getKontonummer().equals(kontonummer3)) {
-                            konto.einzahlen(betrag3);
-                            kontoGefunden = true;
-                            break;
-                        }
-                    }
-                    if (!kontoGefunden) {
-                        System.out.println("Fehler: Konto nicht gefunden.");
-                    }
-                    break;
+                     System.out.println("Kontonummer: ");
+                     String kontonummer3 = scanner.next();
+                     System.out.println("Betrag: ");
+                     double betrag3 = scanner.nextDouble();
+                     kontoGefunden = false;
+                     for (Konto konto : konten) {
+                     if (konto.getKontonummer().equals(kontonummer3)) {
+                     konto.einzahlen(betrag3);
+                     kontoGefunden = true;
+                     break;
+                     }
+                     }
+                     if (!kontoGefunden) {
+                     System.out.println("Fehler: Konto nicht gefunden.");
+                     }
+                     break;
                      **/
                 case 4:
                     /**
-                    System.out.println("Kontonummer: ");
-                    String kontonummer4 = scanner.next();
-                    System.out.println("Betrag: ");
-                    double betrag4 = scanner.nextDouble();
-                    kontoGefunden = false;
-                    for (Konto konto : konten) {
-                        if (konto.getKontonummer().equals(kontonummer4)) {
-                            konto.abheben(betrag4);
-                            kontoGefunden = true;
-                            break;
-                        }
-                    }
-                    if (!kontoGefunden) {
-                        System.out.println("Fehler: Konto nicht gefunden.");
-                    }
-                    break;
-                        **/
+                     System.out.println("Kontonummer: ");
+                     String kontonummer4 = scanner.next();
+                     System.out.println("Betrag: ");
+                     double betrag4 = scanner.nextDouble();
+                     kontoGefunden = false;
+                     for (Konto konto : konten) {
+                     if (konto.getKontonummer().equals(kontonummer4)) {
+                     konto.abheben(betrag4);
+                     kontoGefunden = true;
+                     break;
+                     }
+                     }
+                     if (!kontoGefunden) {
+                     System.out.println("Fehler: Konto nicht gefunden.");
+                     }
+                     break;
+                     **/
                 case 5:
                     /**
-                    System.out.println("Kontonummer: ");
-                    String kontonummer5 = scanner.next();
-                    kontoGefunden = false;
-                    for (Konto konto : konten) {
-                        if (konto.getKontonummer().equals(kontonummer5)) {
-                            konto.kontoauszug();
-                            kontoGefunden = true;
-                            break;
-                        }
-                    }
-                    if (!kontoGefunden) {
-                        System.out.println("Fehler: Konto nicht gefunden.");
-                    }
-                    break;
-                        **/
+                     System.out.println("Kontonummer: ");
+                     String kontonummer5 = scanner.next();
+                     kontoGefunden = false;
+                     for (Konto konto : konten) {
+                     if (konto.getKontonummer().equals(kontonummer5)) {
+                     konto.kontoauszug();
+                     kontoGefunden = true;
+                     break;
+                     }
+                     }
+                     if (!kontoGefunden) {
+                     System.out.println("Fehler: Konto nicht gefunden.");
+                     }
+                     break;
+                     **/
                 case 6:
                     /**
-                    System.out.println("Kontonummer des Absenders: ");
-                    String kontonummer6a = scanner.next();
-                    System.out.println("Kontonummer des Empfängers: ");
-                    String kontonummer6b = scanner.next();
-                    System.out.println("Betrag: ");
-                    double betrag6 = scanner.nextDouble();
-                    boolean kontoGefundenA = false;
-                    boolean kontoGefundenB = false;
-                    Konto kontoA = null;
-                    Konto kontoB = null;
-                    for (Konto konto : konten) {
-                        if (konto.getKontonummer().equals(kontonummer6a)) {
-                            kontoA = konto;
-                            kontoGefundenA = true;
-                        }
-                        if (konto.getKontonummer().equals(kontonummer6b)) {
-                            kontoB = konto;
-                            kontoGefundenB = true;
-                        }
-                        if (kontoGefundenA && kontoGefundenB) {
-                            break;
-                        }
-                    }
-                    if (!kontoGefundenA) {
-                        System.out.println("Fehler: Absender-Konto nicht gefunden.");
-                    } else if (!kontoGefundenB) {
-                        System.out.println("Fehler: Empfänger-Konto nicht gefunden.");
-                    } else {
-                        kontoA.abheben(betrag6);
-                        kontoB.einzahlen(betrag6);
-                        System.out.println("Überweisung erfolgreich.");
-                    }
-                    break;
-                        **/
+                     System.out.println("Kontonummer des Absenders: ");
+                     String kontonummer6a = scanner.next();
+                     System.out.println("Kontonummer des Empfängers: ");
+                     String kontonummer6b = scanner.next();
+                     System.out.println("Betrag: ");
+                     double betrag6 = scanner.nextDouble();
+                     boolean kontoGefundenA = false;
+                     boolean kontoGefundenB = false;
+                     Konto kontoA = null;
+                     Konto kontoB = null;
+                     for (Konto konto : konten) {
+                     if (konto.getKontonummer().equals(kontonummer6a)) {
+                     kontoA = konto;
+                     kontoGefundenA = true;
+                     }
+                     if (konto.getKontonummer().equals(kontonummer6b)) {
+                     kontoB = konto;
+                     kontoGefundenB = true;
+                     }
+                     if (kontoGefundenA && kontoGefundenB) {
+                     break;
+                     }
+                     }
+                     if (!kontoGefundenA) {
+                     System.out.println("Fehler: Absender-Konto nicht gefunden.");
+                     } else if (!kontoGefundenB) {
+                     System.out.println("Fehler: Empfänger-Konto nicht gefunden.");
+                     } else {
+                     kontoA.abheben(betrag6);
+                     kontoB.einzahlen(betrag6);
+                     System.out.println("Überweisung erfolgreich.");
+                     }
+                     break;
+                     **/
                 case 7:
                     scanner.close();
                     System.exit(0);
