da6ae9898c16fbbfbbe6a014ba2183657c6e9c71
Crotear1 <138115674+Crotear1@users.noreply.github.com>
2023-09-18 20:50:52 +0200

    code doku

--- Start/src/Hund.java
+++ Start/src/Hund.java
@@ -1,9 +1,11 @@
 class Hund extends Tier {
+    // Constructor
     public Hund(String name, int age) {
         super(name, age);
     }
-
+    // Methods
     public void gibLaut() {
+        // Output: Wuff
         System.out.println("Wuff");
     }
 }
