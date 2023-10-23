37e8d4ece67273f816cd2f53b06547cb6f247a2b
Crotear1 <138115674+Crotear1@users.noreply.github.com>
2023-10-23 14:44:31 +0200

    added db

--- Start/src/Main.java
+++ Start/src/Main.java
@@ -1,5 +1,29 @@
+import java.sql. * ;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String jdbcUrl = "jdbc:mariadb://localhost:3306/uebungen?user=root&password=123";
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl);
+             Statement statement = connection.createStatement()) {
+
+            String sqlQuery = "SELECT * FROM t_lager";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                Integer name = resultSet.getInt("stueck");
+
+                System.out.println("Name: " + id + " - " + name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
