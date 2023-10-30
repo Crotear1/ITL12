9c1017941fd38e9d341b9ef93dd9b7e6d2e0b3b8
Crotear1 <138115674+Crotear1@users.noreply.github.com>
2023-10-30 16:19:03 +0100

    show table

--- Start/src/Buchhaltung.java
+++ Start/src/Buchhaltung.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.sql. * ;
+import java.util.ArrayList;
 
 public class Buchhaltung {
     private JTable table1;
@@ -7,21 +10,49 @@ public class Buchhaltung {
     private JTextField textField5;
     private JTextField textField6;
     private JButton buchenButton;
+    private JList list1;
+
+    private DefaultTableModel tableModel = new DefaultTableModel();
+    private ArrayList<String> tableData;
+
+    public Buchhaltung(){
+        buchenButton.addActionListener(e -> {
+            // get the values from the text fields
+            // create a new transaction object
+            // save the transaction to the database
+            // update the table
+            ladeTransaktionen();
+        });
+
+        table1.setModel(tableModel);
+        tableModel.addColumn("TransaktionsID");
+        tableModel.addColumn("Betrag");
+        tableModel.addColumn("Datum");
+        tableModel.addColumn("Kategorie");
+        tableModel.addColumn("Beschreibung");
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Buchhaltung");
-        frame.setContentPane(new Buchhaltung().table1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        Buchhaltung buchhaltung = new Buchhaltung();
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Buchhaltung");
+            frame.setContentPane(buchhaltung.table1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        });
 
         Datenbank datenbank = new Datenbank();
-        datenbank.verbindeUndFühreAbfrageAus();
+        datenbank.verbindeDatenbank();
+        buchhaltung.ladeTransaktionen();
     }
 
     public void ladeTransaktionen() {
-        // select all transactions and display them in the table
-
+        // get all data from the database and put it into the table
+        Datenbank datenbank = new Datenbank();
+        this.tableData = datenbank.getTransaktionen();
+        tableModel.addRow(this.tableData.toArray());
 
     }
+
 }
--- Start/src/Datenbank.java
+++ Start/src/Datenbank.java
@@ -1,42 +1,59 @@
+import javax.swing.table.DefaultTableModel;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datenbank {
     private Connection connection;
 
     public Datenbank() {
         // Konstruktor: Hier können Sie die Datenbankverbindung initialisieren
+        verbindeDatenbank();
     }
 
-    public void verbindeUndFühreAbfrageAus() {
+    public void verbindeDatenbank() {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             String jdbcUrl = "jdbc:mariadb://localhost:3306/buchhaltung?user=root&password=123";
             connection = DriverManager.getConnection(jdbcUrl);
 
-            String sqlQuery = "SELECT * FROM Transaktionen";
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getTransaktionen() {
+        try {
+            String sqlQuery = "SELECT TransaktionsID, Betrag FROM Transaktionen";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlQuery);
 
+            ArrayList<String> transaktionen =  new ArrayList<>();
             while (resultSet.next()) {
                 Integer id = resultSet.getInt("TransaktionsID");
                 Double betrag = resultSet.getDouble("Betrag");
-
-                System.out.println("TransaktionsID: " + id + ", Betrag: " + betrag);
+                transaktionen.add("TransaktionsID: " + id + ", Betrag: " + betrag);
             }
-        } catch (ClassNotFoundException | SQLException e) {
+            return transaktionen;
+        } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+        }
+        return  new ArrayList<>();
+    }
+
+    public void schließeVerbindung() {
+        try {
+            if (connection != null) {
+                connection.close();
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
+
+
 }
